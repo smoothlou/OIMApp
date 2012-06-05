e9cfb7dedccaf39dce142cc4f56d98e89949202b
Luis Rodriguez <smoothlou@gmail.com>
2012-06-05 11:37:28 -0500

    Figured out log in and sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,34 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  def new
-    @contact = Contact.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @contact }
-    end
-  end
-
-  # GET /contacts/1/edit
-  # POST /contacts
-  # POST /contacts.json
-  def create
-    @contact = Contact.new(params[:contact])
-    if params[:keyword].present?
-      @new = Contact.new(:first_name => params[:keyword])
-      @new.save
-      redirect_to @contact
-    end
-
-    respond_to do |format|
-      if @contact.save
-        format.html { redirect_to @contact, notice: 'Contact was successfully created.' }
-        format.json { render json: @contact, status: :created, location: @contact }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @contact.errors, status: :unprocessable_entity }
-      end
-    end
-  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,28 @@ class UsersController < ApplicationController
 
 def new
  @user = User.new
+ 
+ respond_to do |format|
+   format.html #new.html.erb
+   format.json { render json: @user }
+end
 end
 
+def edit
+  @user = User.find(params[:id])
+end
 
+def create
+  @user = User.new(params[:user])
+
+  respond_to do |format|
+    if @user.save
+      format.html {redirect_to root_url, notice: 'Thanks for signing up!'}
+      format.json { render json: @user, status: :created, location: @user }
+    else
+      format.html { render action: "new" }
+      format.json { render json: @user.errors, status: :unprocessable_entity }
+    end
+  end
+end
 end
